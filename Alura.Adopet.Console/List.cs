9fd021df94c0eade338c3a6411e816e5fc40aad1
Marcos Paulo <marcos.poo@gmail.com>
2024-04-25 23:13:49 -0300

    Aula 1 - Melhorando a legibilidade do código
    Nessa aula:
    - Entendemos como o sistema da AdoPet funciona;
    - Percebemos o quanto utilizar parâmetro nomeado pode reduzir tempo de desenvolvimento e linhas de código e evitar assim o uso excessivo de comentários;
    - Como as extrações de classes podem nos ajudar no processo de organização da nossa aplicação e assim evitar um grande número de linhas de código do program.cs.

---
+++ Alura.Adopet.Console/List.cs
@@ -0,0 +1,39 @@
+﻿using System.Net.Http.Headers;
+using System.Net.Http.Json;
+
+namespace Alura.Adopet.Console
+{
+    public class List
+    {
+        HttpClient client;
+
+        public List()
+        {
+            this.client = ConfiguraHttpClient("http://localhost:5057");
+        }
+
+        public async Task ListarPets()
+        {
+            var pets = await ListPetsAsync();
+            foreach (var pet in pets)
+            {
+                System.Console.WriteLine(pet);
+            }
+        }
+
+        HttpClient ConfiguraHttpClient(string url)
+        {
+            HttpClient _client = new HttpClient();
+            _client.DefaultRequestHeaders.Accept.Clear();
+            _client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            _client.BaseAddress = new Uri(url);
+            return _client;
+        }
+
+        async Task<IEnumerable<Pet>?> ListPetsAsync()
+        {
+            HttpResponseMessage response = await client.GetAsync("pet/list");
+            return await response.Content.ReadFromJsonAsync<IEnumerable<Pet>>();
+        }
+    }
+}
---
+++ Alura.Adopet.Console/Show.cs
@@ -0,0 +1,24 @@
+﻿namespace Alura.Adopet.Console
+{
+    public class Show
+    {
+        public void ExibirArquivo(string caminhoDoArquivoASerExibido)
+        {
+            using (StreamReader sr = new StreamReader(caminhoDoArquivoASerExibido))
+            {
+                System.Console.WriteLine("----- Serão importados os dados abaixo -----");
+                while (!sr.EndOfStream)
+                {
+                    // separa linha usando ponto e vírgula
+                    string[] propriedades = sr.ReadLine().Split(';');
+                    // cria objeto Pet a partir da separação
+                    Pet pet = new Pet(Guid.Parse(propriedades[0]),
+                    propriedades[1],
+                    TipoPet.Cachorro
+                    );
+                    System.Console.WriteLine(pet);
+                }
+            }
+        }
+    }
+}
